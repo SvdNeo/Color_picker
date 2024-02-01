53e6d2a456c12b5a0b5e9722ca4cac72e0717208
SvdNeo <sanjay.d@neoito.com>
2024-02-01 17:48:39 +0530

    now the app is built with minimum required functionality

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const browseBtn = document.getElementById('browseImage');
 const uploadedImage = document.getElementById('uploadedImage');
+const addBtn = document.getElementById('add'); 
+const removeBtn = document.getElementById('remove');
+const paletteContainer = document.querySelector('.dynamic-container');
+const image = document.getElementById('uploadedImage');
+const exportBtn = document.getElementById('exportPalette');
 
 browseBtn.addEventListener('click', () => {
     const fileInput = document.createElement('input');
@@ -13,3 +18,70 @@ browseBtn.addEventListener('click', () => {
   
     fileInput.click();
 });
+
+let palette = [];
+
+const eyeDropper = new EyeDropper();
+
+addBtn.addEventListener('click', async () => {
+    if (palette.length >= 10) {
+      addBtn.disabled = true;
+      return;
+    }
+  
+    const { sRGBHex } = await eyeDropper.open();
+    palette.push(sRGBHex);
+    const swatch = document.createElement('div');
+    swatch.classList.add('color-swatch');
+    swatch.style.backgroundColor = sRGBHex;
+    paletteContainer.appendChild(swatch);
+    
+    if(palette.length < 10) {
+      addBtn.disabled = false;
+    }
+});
+
+removeBtn.addEventListener('click', () => {
+    palette.pop();
+    paletteContainer.lastChild.remove();
+    
+    if (palette.length < 10) {
+      addBtn.disabled = false;
+    }
+});
+
+function generateCSV() {
+    const capitalizedPalette = palette.map(color => color.toUpperCase().replace('#', ''));
+    return capitalizedPalette.join(',');
+}
+
+exportBtn.addEventListener('click', () => {
+    const csvContent = generateCSV();
+    const withHashContent = generateWithHash();
+    const arrayContent = generateArray();
+  
+    const modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  
+    const modalContent = document.querySelector(".modal-content");
+    modalContent.innerHTML = `
+      <span class="close">&times;</span>
+      <h2>Export Palette</h2>
+      <div id="csvFormat">- CSV<br>${csvContent}</div>
+      <div id="withHashFormat">- With #<br>${withHashContent}</div>
+      <div id="arrayFormat">- Array<br>${arrayContent}</div>
+    `;
+  
+    const closeBtn = document.querySelector(".close");
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+});
+
+function generateWithHash() {
+    return palette.map(color => color.toUpperCase()).join(',');
+}
+
+function generateArray() {
+    return "[" + palette.map(color => "'" + color.toUpperCase().replace('#', '') + "'").join(', ') + "]";
+}
