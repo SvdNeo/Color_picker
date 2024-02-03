ded0c1a977b9c274ae51d40ada3ba916f6bc77a7
SvdNeo <sanjay.d@neoito.com>
2024-02-03 21:56:47 +0530

    created function to addcolorpicker

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ function handleImageUpload(event) {
   }
 }
 
+function addColorPicker() {
+  if (colorPickers.length < 10) {
+    const colorPicker = document.createElement("div");
+    colorPicker.classList.add("colorPicker");
+    colorPicker.style.top = getRandomPosition() + "px";
+    colorPicker.style.left = getRandomPosition() + "px";
+
+    colorPicker.addEventListener("mousedown", startDrag);
+    colorPickers.push(colorPicker);
+    updateColorPalette();
+    document.getElementById("canvasContainer").appendChild(colorPicker);
+    addActivePicker(colorPicker);
+
+    if (colorPickers.length === 10) {
+      document.getElementById("addColorPickerButton").disabled = true;
+    }
+  } else {
+    alert("You have reached the maximum number of color pickers");
+  }
+}
