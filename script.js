2c08bb81a4e6765e80e60a78f17ec75b2df1c359
SvdNeo <sanjay.d@neoito.com>
2024-02-04 17:02:31 +0530

    created the default image function to load the default image onload

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ const colorPaletteContainer = document.getElementById("colorPalette");
 const imageInput = document.getElementById("imageInput");
 let colorPickers = [];
 let hexValues = [];
+
 imageInput.addEventListener("change", handleImageUpload);
+const defaultImg = new Image();
+defaultImg.src = "/default_image.jpg";
+let initialLoad = true;
+defaultImg.onload = function() {
+  imageCanvas.getContext("2d").drawImage(defaultImg, 0, 0, imageCanvas.width, imageCanvas.height);
+  if (initialLoad) {
+    addColorPicker();
+    addColorPicker();
+    initialLoad = false; 
+  }
+}
+
+defaultImg.crossOrigin = "Anonymous";
+
 
 function handleImageUpload(event) {
   const file = event.target.files[0];
@@ -13,24 +28,21 @@ function handleImageUpload(event) {
 
     reader.onload = function (e) {
       const img = new Image();
-      if (!e.target.result) {
-        img.src = "/default_image.jpg";
-      } else {
-        img.src = e.target.result;
-      }
+    
 
       img.onload = function () {
         imageCanvas
           .getContext("2d")
           .drawImage(img, 0, 0, imageCanvas.width, imageCanvas.height);
-        addColorPicker();
-        addColorPicker();
-      };
+             };
+   
+      img.src = e.target.result;
     };
 
     reader.readAsDataURL(file);
-  }
+  } 
 }
+
 
 function addColorPicker() {
   if (colorPickers.length < 10) {
@@ -41,14 +53,33 @@ function addColorPicker() {
 
     colorPicker.addEventListener("mousedown", startDrag);
     colorPickers.push(colorPicker);
+    console.log(colorPickers);
     updateColorPalette();
     document.getElementById("canvasContainer").appendChild(colorPicker);
     addActivePicker(colorPicker);
 
+  
     if (colorPickers.length === 10) {
+    
       document.getElementById("addColorPickerButton").disabled = true;
     }
   } else {
-    alert("You have reached the maximum number of color pickers");
+    document.getElementById("addColorPickerButton").disabled = false;
   }
 }
+
+function removeColorPicker() {
+  if (colorPickers.length > 2) {
+   
+    const removedPicker = colorPickers.pop();
+    removedPicker.remove();
+
+    hexValues.pop();
+    updateColorPalette();
+   
+    document.getElementById("addColorPickerButton").disabled = false;
+  } else {
+    document.getElementById("removeColorPickerButton").disabled = true;
+  }
+}
+
